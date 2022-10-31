7b1ff9eaaed4f59722c41a957e7a59880a88e0f7
personalcurso <cursocurso394@gmail.com>
2022-10-31 09:38:14 -0500

    cambios a la rama nueva

--- archivo2.py
+++ archivo2.py
@@ -4,13 +4,5 @@ def funcionmodificar():
     suma=4+5
     print(suma)
 
-def funcionsoloenRamaprueba():
-    print("v")
-    print("ok")
-    
-def lineanueva():
-    print("soy nuevo")
 
 
-def soloverloenramaprueba():
-    pass
